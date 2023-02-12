bf5b38fdb1cf1e733b9dc0579b337958f77e24fd
programmer-liao <2380167274@qq.com>
2023-02-12 21:31:59 +0800

    feat:xuecheng-plus-gateway启动

---
+++ xuecheng-plus-gateway/src/main/java/com/dezhi/gateway/XueChengPlusGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.dezhi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liaodezhi
+ * @date 2023/2/12
+ */
+@SpringBootApplication
+public class XueChengPlusGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XueChengPlusGatewayApplication.class, args);
+    }
+}
